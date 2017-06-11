60b261792e49646655bc33f65b0dbcd6f158fd3d
Siddharth Prabhu <siddharth.prabhu.mec12@iitbhu.ac.in>
2017-06-11 11:30:53 +0530

    R code to find the roots of a quadratic equation (with all cases covered)

---
+++ quadRoots.R
@@ -0,0 +1,21 @@
+# Constructing Quadratic Formula
+quadRoots <- function(a,b,c){
+  if(delta(a,b,c) > 0){ # first case D>0
+    x_1 = (-b+sqrt(delta(a,b,c)))/(2*a)
+    x_2 = (-b-sqrt(delta(a,b,c)))/(2*a)
+    result = c(x_1,x_2)
+  }
+  else if(delta(a,b,c) == 0){ # second case D=0
+    result = -b/(2*a)
+  }
+  else {
+    result <- "There are no real roots."
+    } # third case D<0
+  
+  result
+}
+
+# Constructing delta
+delta<-function(a,b,c){
+  b^2-4*a*c
+}
